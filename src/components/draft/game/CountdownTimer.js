3c008eacf9dc3f8e08ed37c2203f6d3989f14ecb
Alex Boyd <malexanderboyd@gmail.com>
2020-06-20 12:26:16 -0400

    remove comments

--- src/components/draft/game/CountdownTimer.js
+++ src/components/draft/game/CountdownTimer.js
@@ -8,12 +8,6 @@ const CountdownTimer = ({timerSettings, setTimeUp}) => {
     yellowPhaseStart = Math.floor(seconds * .50)
     redPhaseStart = Math.floor(seconds * .25)
 
-    // [CountdownTimerComponent] useEffect seconds: 5 CountdownTimer.js:15:16
-    // received timer seconds for next round 3 DraftGame.js:205:28
-    // create new timer at 3 DeckList.js:383:20
-    // [CountdownTimerComponent] useEffect seconds: 3 CountdownTimer.js:15:16
-    // received timer seconds for next round 3 6 DraftGame.js:205:28
-
     useEffect(() => {
         setTimeUp(false)
         // we wrap this in an object so useState will update if the previous seconds == this seconds
@@ -21,7 +15,6 @@ const CountdownTimer = ({timerSettings, setTimeUp}) => {
         yellowPhaseStart = Math.floor(seconds * .50)
         redPhaseStart = Math.floor(seconds * .25)
         setTimeLeft(seconds)
-        console.log("[CountdownTimerComponent] useEffect seconds: " + seconds)
     }, [timerSettings])
 
     useEffect(() => {
